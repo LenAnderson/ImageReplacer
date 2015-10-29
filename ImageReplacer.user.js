bd87d16bae658f3a66ae64d2a2d95a3492d12b89
Len <LenAnderson@users.noreply.github.com>
2015-10-29 10:59:37 +0100

    Added MutationObserver

--- ImageReplacer.user.js
+++ ImageReplacer.user.js
@@ -2,7 +2,7 @@
 // @name         Image Replacer
 // @namespace    https://github.com/LenAnderson/
 // @downloadURL  https://github.com/LenAnderson/ImageReplacer/raw/master/ImageReplacer.user.js
-// @version      1.0
+// @version      1.1
 // @description  Replace selected images with other images.
 // @author       LenAnderson
 // @match        *://*/*
@@ -16,23 +16,25 @@ subs.forEach(function(it) {
 });
 
 Array.prototype.forEach.call(document.querySelectorAll('img'), function(img) {
-    img.addEventListener('contextmenu', function(evt) {
-        if (evt.ctrlKey) {
-            evt.preventDefault();
-            if (img.getAttribute('data-image-replacer')) {
-                if (confirm('Do you want to stop replacing this image?')) {
-                    subs.splice(subs.lastIndexOf(img.src), 1);
-                    GM_setValue('image-replacer', JSON.stringify(subs));
-                    Array.prototype.forEach.call(document.querySelectorAll('img[data-image-replacer-original="' + img.getAttribute('data-image-replacer-original') + '"]'), unreplace);
-                }
-            } else if (confirm('Do you want to replace this image?')) {
-                subs.push(img.getAttribute('src'));
-                GM_setValue('image-replacer', JSON.stringify(subs));
-                Array.prototype.forEach.call(document.querySelectorAll('img[src="' + img.getAttribute('src') + '"]'), replace);
-            }
-        }
-    });
+    img.addEventListener('contextmenu', listener);
 });
+
+function listener(evt) {
+    if (evt.ctrlKey) {
+        evt.preventDefault();
+        if (this.getAttribute('data-image-replacer')) {
+            if (confirm('Do you want to stop replacing this image?')) {
+                subs.splice(subs.lastIndexOf(this.src), 1);
+                GM_setValue('image-replacer', JSON.stringify(subs));
+                Array.prototype.forEach.call(document.querySelectorAll('img[data-image-replacer-original="' + this.getAttribute('data-image-replacer-original') + '"]'), unreplace);
+            }
+        } else if (confirm('Do you want to replace this image?')) {
+            subs.push(this.getAttribute('src'));
+            GM_setValue('image-replacer', JSON.stringify(subs));
+            Array.prototype.forEach.call(document.querySelectorAll('img[src="' + this.getAttribute('src') + '"]'), replace);
+        }
+    }
+}
 
 function replace(img) {
     img.setAttribute('data-image-replacer', '//www.illmurray.com/g/');
@@ -44,3 +46,28 @@ function unreplace(img) {
     img.removeAttribute('data-image-replacer');
     img.removeAttribute('data-image-replacer-original');
 }
+
+
+var mo = new MutationObserver(function(muts) {
+    muts.forEach(function(mut) {
+        Array.prototype.forEach.call(mut.addedNodes, function(node) {
+            console.info('added node', node, node instanceof HTMLElement, node.tagName);
+            if (node instanceof HTMLElement) {
+                if (node.tagName == 'IMG') {
+                    node.addEventListener('contextmenu', listener);
+                    if (subs.lastIndexOf(node.getAttribute('src')) != -1) {
+                        replace(node);
+                    }
+                } else {
+                    Array.prototype.forEach.call(node.querySelectorAll('img'), function(img) {
+                        img.addEventListener('contextmenu', listener);
+                        if (subs.lastIndexOf(img.getAttribute('src')) != -1) {
+                            replace(img);
+                        }
+                    });
+                }
+            }
+        });
+    });
+});
+mo.observe(document.body, {childList: true, subtree: true});
